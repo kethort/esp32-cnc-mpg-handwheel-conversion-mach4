9b4a147614e5adc5d6f2bd5d02e9cf0ca38add67
kethort <10733500+kethort@users.noreply.github.com>
2020-10-07 20:16:32 -0700

    Update ESP32Mach4Modbus.ino

--- ESP32Mach4Modbus/ESP32Mach4Modbus.ino
+++ ESP32Mach4Modbus/ESP32Mach4Modbus.ino
@@ -5,51 +5,74 @@
 #include <ModbusIP_ESP8266.h>
 #include <ESP32Encoder.h>
 
+#define STATIC_IP
+
 ModbusIP mb;
-int regs[] = {1, 2, 3, 4, 5, 6, 7}; 
-int pins[] = {13, 14, 27, 25, 26, 23, 19};
+int regs[] = {1,  2,  3,  9,  10, 11, 4,  5,  6,  7}; 
+int pins[] = {13, 14, 27, 32, 35, 34, 25, 15, 21, 22};
+
+byte currentPin;
 
 ESP32Encoder encoder;
 
 void setup() {
   Serial.begin(115200);
- 
+  
   WiFiManager wifiManager;
 
+#ifdef STATIC_IP
   IPAddress _ip = IPAddress(192, 168, 1, 222);
   IPAddress _gw = IPAddress(192, 168, 1, 1);
   IPAddress _sn = IPAddress(255, 255, 255, 0);
   
   wifiManager.setSTAStaticIPConfig(_ip, _gw, _sn);
+#endif
   
-  wifiManager.autoConnect("AutoConnectAP");
+  wifiManager.autoConnect("MPG_Handwheel");
 
   Serial.println("WiFi connected");  
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
         
   mb.server();
-  
+
   for(int i = 0; i < sizeof(regs)/sizeof(regs[0]); i++) {
     pinMode(pins[i], INPUT_PULLUP);
     mb.addCoil(regs[i]); 
   } 
 
+  // inputs 2 and 4 are A+ and B+ on the MPG Handwheel
   encoder.attachFullQuad(2, 4);
   encoder.clearCount();
-  mb.addHreg(55);
+  mb.addHreg(55); // encoder counts modbus register
 }
  
 void loop() {
   mb.task();
-   
+  
   for(int i = 0; i < sizeof(regs)/sizeof(regs[0]); i++)
+  {
+    // clear the encoder count if the axis input changes
+    if (!digitalRead(pins[i]) && i < 6 && currentPin != pins[i]) {
+        currentPin = pins[i];
+        encoder.clearCount();
+        mb.Hreg(55, 0);
+    }
+    
     mb.Coil(regs[i], !digitalRead(pins[i]));
+  }
 
-  if(!digitalRead(25))
-    mb.Hreg(55, (int16_t)encoder.getCount());
-  else {
+  // forces encoder count rollover to zero instead of int16_t max/min
+  if((int16_t)encoder.getCount() > 32760 ||(int16_t)encoder.getCount() < -32760){
     encoder.clearCount();
-    mb.Hreg(55, 0); 
+    mb.Hreg(55, 0);
+  }
+
+  // only get encoder counts when the MPG enable button is pressed
+  if(!digitalRead(25)) {
+    encoder.resumeCount();
+    mb.Hreg(55, (int16_t)encoder.getCount());
+  } else {
+    encoder.pauseCount();
   }
 }
