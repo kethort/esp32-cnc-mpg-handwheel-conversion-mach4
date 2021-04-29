b29ee3df718ba0a3ae8cc2738d63f1244e7ca1c9
kethort <10733500+kethort@users.noreply.github.com>
2021-04-28 23:12:36 -0700

    added better comments in MPGInstaller.py

--- src/installer/MPGInstaller.py
+++ src/installer/MPGInstaller.py
@@ -9,10 +9,14 @@ import xml.etree.ElementTree as et
 from subprocess import Popen, PIPE
 
 class AppWindow(wx.Frame):
-	MAC_VENDOR_ID = '86-1a-30'
+	MAC_VENDOR_ID = '8d-ec-a0'
 	dir_changed = False
 
 	def __init__(self):
+		'''
+			initializes the wxWidgets frame, binds events to objects in the form 
+			and auto-populates some form input 
+		'''
 		wx.Frame.__init__(self, None, id = wx.ID_ANY, title = 'Wireless MPG Installer', pos = wx.DefaultPosition, size = wx.Size(500,233), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL)
 
 		self.SetSizeHints(wx.DefaultSize, wx.DefaultSize)
@@ -100,6 +104,14 @@ class AppWindow(wx.Frame):
 		self.find_default_mach_installation()
 
 	def get_device_ip(self):
+		'''
+			searches the network for the ESP32 (wireless MPG) ip address
+			based on the mac address specified in MAC_VENDOR_ID and populates the ip 
+			address textbox in the wxForm.
+
+			function uses arp -a command in windows so if the device is not
+			in the arp table it will not be found 
+		'''
 		ip_addr = ''
 		pid = Popen(['arp', '-a'], stdout=PIPE)
 		cmd_out = pid.communicate()[0].decode('utf-8').split()
@@ -116,6 +128,11 @@ class AppWindow(wx.Frame):
 		return ip_addr
 
 	def find_default_mach_installation(self):
+		'''
+			searches the root directory in Windows for any Mach4 installation
+			and populates the text box in the wxForm with the first found Mach4
+			installation directory.
+		'''
 		root_dir = os.path.abspath(os.sep)
 		for folder in os.listdir(root_dir):
 			if 'Mach4' in folder:
@@ -124,6 +141,10 @@ class AppWindow(wx.Frame):
 				break
 
 	def update_profile_choices(self, event):
+		'''
+			extracts the names of each of the profiles in the Mach4 Profiles directory
+			and populates the dropdown box in the wxForm.
+		'''
 		if self.dir_changed:
 			self.dir_changed = False
 			mach_dir = self.mach_dir_url.GetValue()
@@ -139,6 +160,10 @@ class AppWindow(wx.Frame):
 		event.Skip()
 
 	def btn_browse_on_click(self, event):
+		'''
+			browse button logic for manually selecting the Mach4
+			installation directory.
+		'''
 		dlg = wx.DirDialog(None, "Choose Mach4 installation directory", "", wx.DD_DEFAULT_STYLE|wx.DD_DIR_MUST_EXIST)
 		res = dlg.ShowModal()
 		if res == wx.ID_OK:
@@ -146,18 +171,15 @@ class AppWindow(wx.Frame):
 			self.mach_dir_url.ChangeValue(file_path)
 			self.dir_changed = True
 		event.Skip()
-
-	def dict_to_machine_ini(self, ini_dict, out_loc):
-		with open(out_loc, 'w') as out_file:
-			for item in ini_dict:
-				out_file.write('[{0}]\n'.format(item))
-				properties_dict = ini_dict[item]
-
-				if properties_dict:
-					for prop in properties_dict: 
-						out_file.write('{0}={1}\n'.format(prop, properties_dict[prop]))
 	
 	def machine_ini_to_dict(self, ini_loc):
+		'''
+			transforms a Mach4 Machine.ini configuration file into a 
+			python dictionary
+
+			params -
+				ini_loc: the directory path to the Mach4 Machine.ini configuration file
+		'''
 		main_dict = {}
 		nested_dict = {}
 		heading = ''
@@ -190,7 +212,33 @@ class AppWindow(wx.Frame):
 
 		return main_dict
 
+	def dict_to_machine_ini(self, ini_dict, out_loc):
+		'''
+			transforms a python dictionary into a Mach4 Machine.ini
+			configuration file
+
+			params -
+				ini_dict: the Mach4 Machine.ini in python dictionary format
+				
+				out_loc: the directory path to save the Mach4 Machine.ini file
+		'''
+		with open(out_loc, 'w') as out_file:
+			for item in ini_dict:
+				out_file.write('[{0}]\n'.format(item))
+				properties_dict = ini_dict[item]
+
+				if properties_dict:
+					for prop in properties_dict: 
+						out_file.write('{0}={1}\n'.format(prop, properties_dict[prop]))
+
 	def is_valid_ipv4_address(self, address):
+		'''
+			checks if the ip address entered in the wxForm is valid
+			upon submission
+
+			params -
+				address: the ip address as a string in format xxx.xxx.xxx.xxx
+		'''
 		try:
 		    socket.inet_pton(socket.AF_INET, address)
 		except AttributeError:  # no inet_pton here, sorry
@@ -205,7 +253,10 @@ class AppWindow(wx.Frame):
 		return True
 
 	def mach_is_running(self):
-		# check if Mach4 is running
+		'''
+			checks if the Mach4 program is currently running. blocks installation
+			if true.
+		'''
 		mach_running = False
 
 		for process in psutil.process_iter():
@@ -216,7 +267,14 @@ class AppWindow(wx.Frame):
 		return mach_running
 
 	def backup_machine_ini(self, ini_path, backup_path):
-		# make a backup of Machine.ini
+		'''
+			creates a backup of Mach4 Machine.ini configuration file
+
+			params -
+				ini_path: the directory path to the Mach4 Machine.ini configuration file
+				
+				backup_path: the directory path to save the backup to
+		'''
 		if self.cbo_profile_selection.GetSelection() != 0:			
 			try:
 				shutil.copy(ini_path, backup_path)
@@ -224,12 +282,29 @@ class AppWindow(wx.Frame):
 				print('failed to copy file \n {0}'.format(e))
 
 	def enable_plugin_in_ini(self, ini_dict, plugin_name):
+		'''
+			sets the value of a desired Mach4 Plugin to be enabled to 1 in the 
+			Mach4 Machine.ini configuration file 
+
+			params -
+				ini_dict: the Mach4 Machine.ini in python dictionary format
+				
+				plugin_name: the name of the Mach4 plugin to enable
+		'''
 		test_val = ini_dict.get('Plugins', None)
 		if test_val is None:
 			ini_dict['Plugins'] = {}
 		ini_dict['Plugins'][plugin_name] = 1
 
 	def create_modbus_device(self, ini_dict, dev_name):
+		'''
+			creates a modbus device in the Mach4 Machine.ini configuration file
+
+			params -
+				ini_dict: the Mach4 Machine.ini in python dictionary format
+
+				dev_name: desired name of the modbus device
+		'''
 		dev_id = 0
 		if 'ModbusDevices' in ini_dict:
 			if ini_dict['ModbusDevices'] is not None:
@@ -245,6 +320,12 @@ class AppWindow(wx.Frame):
 		ini_dict['ModbusDevice'] = None
 
 	def create_global_registers(self, ini_dict): 
+		'''
+			creates a global register in the Mach4 Machine.ini configuration file
+
+			params -
+				ini_dict: the Mach4 Machine.ini in python dictionary format
+		'''
 		inc = 1
 		# find the last index of gReg attribute
 		for properties in ini_dict:
@@ -266,27 +347,36 @@ class AppWindow(wx.Frame):
 
 
 	def btn_install_on_click(self, event):
+		'''
+			Install button form submit. 
+
+			Performs the installation of all the necessary file configurations in Mach4, to enable 
+			modbus tcp/ip communication between the ESP32 MPG Handwheel and the Mach4 software 
+		'''
 		mach_dir = self.mach_dir_url.GetValue()
 		ini_path = backup_path = ''
 		profile = self.cbo_profile_selection.GetValue()
 		ini_path = os.path.join(mach_dir, 'Profiles\\{0}\\Machine.ini'.format(profile))
 		backup_path = os.path.join(mach_dir, 'Profiles\\{0}\\Backups\\Machine.ini.bak'.format(profile))
 		
+		# exit the program and notify the user if Mach4 is currently running or if
+		# the entered IP address is invalid
 		if self.mach_is_running():
 			return
 
-		# validate IP address 
 		ip_addr = self.ip_address_entry.GetValue().strip()
 
 		if not self.is_valid_ipv4_address(ip_addr):
 			wx.MessageBox("Invalid IP Address Entered! \n Aborting Installation.", "Error", wx.OK)
 			return
 
+		# backup the Mach4 Machine.ini configuration file
 		self.backup_machine_ini(ini_path, backup_path)
 
+		# convert the Mach4 Machine.ini configuration file into a python dicitonary
 		ini_dict = self.machine_ini_to_dict(ini_path)
 		 
-		#  enable modbus, regfile and LUA plugins
+		#  enable the modbus, regfile, and LUA plugins in the Mach4 Machine.ini configuration file
 		self.enable_plugin_in_ini(ini_dict, 'mcRegfile')
 		self.enable_plugin_in_ini(ini_dict, 'mcModbus')
 		self.enable_plugin_in_ini(ini_dict, 'mcLua')
@@ -304,13 +394,14 @@ class AppWindow(wx.Frame):
 
 		#self.create_global_registers(ini_dict)
 
-		# add ip address to ini dict
+		# add the ip address to the Mach4 Machine.ini configuration file 
+		# under the created modbus device
 		ini_dict['ModbusDevice/ModbusMPG']['IPAddr'] = ip_addr
 
-		# disable MPG 11 in Machine.ini
+		# disable MPG 11 in Mach4 Machine.ini configuration file
 		ini_dict['Mpg11']['Enabled'] = 0
 
-		# edit Mach4 screen to include requirements
+		# makes a copy of the Mach4 Screen and extracts it's contents into a tmeporary directory
 		screens_dir = os.path.join(mach_dir, 'Screens')
 		screen_name = ini_dict['Preferences']['Screen'].strip()
 		screen_path = os.path.join(screens_dir, screen_name)
@@ -325,7 +416,8 @@ class AppWindow(wx.Frame):
 		with zipfile.ZipFile(screen_path, 'r') as zip_ref:
 			zip_ref.extractall(temp_dir)
 
-		# programmatically write code in the Screen Load and PLC Scripts to connect the ModbusMPG module
+		# programmatically write LUA code into the Screen Load and PLC Scripts to connect the ModbusMPG module to
+		# the ESP32 MPG Handwheel inputs and outputs
 		screen_load = '\n\n--ModbusMPG module\npackage.loaded.ModbusMPG = nil\nModbusMPG = require \"ModbusMPG\"\n'
 		plc = '\n\nModbusMPG.RunModbusMPG()\n' 
 		tree = et.parse(os.path.join(temp_dir, 'screen.xml'))
