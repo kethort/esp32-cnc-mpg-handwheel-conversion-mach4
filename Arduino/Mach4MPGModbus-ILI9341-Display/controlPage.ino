eade0de1f21cd74404a009af8f2ca2dffa873c0d
kethort <10733500+kethort@users.noreply.github.com>
2022-03-10 01:34:27 -0800

    adding buttons to control page

--- Arduino/Mach4MPGModbus-ILI9341-Display/controlPage.ino
+++ Arduino/Mach4MPGModbus-ILI9341-Display/controlPage.ino
@@ -1,8 +1,27 @@
+#define ENABLEBUTTON_X 5
+#define ENABLEBUTTON_Y 5
+
+#define HOMEBUTTON_X 85
+#define HOMEBUTTON_Y 5
+
+#define STARTBUTTON_X 164
+#define STARTBUTTON_Y 5
+
+#define PAUSEBUTTON_X 5
+#define PAUSEBUTTON_Y 65
+
+#define AXESBUTTON_X 85
+#define AXESBUTTON_Y 65
+
+#define BATTBUTTON_X 164
+#define BATTBUTTON_Y 65
+
 void drawControlPage() {
   pageNum = 2;
 
   tft.fillScreen(TFT_BLACK);
-  
+
+  drawControlButtons();
   drawButtonOnScreen("Back", BACKBUTTON_X, BACKBUTTON_Y, MIDBUTTON_W, MIDBUTTON_H);
 }
 
@@ -21,4 +40,13 @@ void getTouchControlPage() {
       drawDROPage();
     }  
   }
+}
+
+void drawControlButtons() {
+  drawButtonOnScreen("ENABLE", ENABLEBUTTON_X, ENABLEBUTTON_Y, MIDBUTTON_W, MIDBUTTON_H);
+  drawButtonOnScreen("HOME", HOMEBUTTON_X, HOMEBUTTON_Y, MIDBUTTON_W, MIDBUTTON_H);
+  drawButtonOnScreen("START", STARTBUTTON_X, STARTBUTTON_Y, MIDBUTTON_W, MIDBUTTON_H);
+  drawButtonOnScreen("PAUSE", PAUSEBUTTON_X, PAUSEBUTTON_Y, MIDBUTTON_W, MIDBUTTON_H);
+  drawButtonOnScreen("AXES", AXESBUTTON_X, AXESBUTTON_Y, MIDBUTTON_W, MIDBUTTON_H);
+  drawButtonOnScreen("BATT", BATTBUTTON_X, BATTBUTTON_Y, MIDBUTTON_W, MIDBUTTON_H);
 }
